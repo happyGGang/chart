a024171bc5d921a3bcdbb7e310b32860b33c0b3d
happyGGang <joy.gang@dxworks.kr>
2022-10-26 22:45:29 +0900

    [update] 기본 옵션값 생성

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 ;(function () {
   'use strict'
+
+  const get = (element) => {
+    document.querySelector(element)
+  }
+
+  class Chart {
+    constructor() {
+
+    }
+  }
+
+  const data = {
+    guitar: 30,
+    bass: 20,
+    drun: 18,
+    piano: 18
+  }
+
+  const option = {
+    radius: 150,
+    width: 700,
+    hetghit: 500,
+    colors: ['#c15454', '#6fd971', '#687bd2', '#b971e0']
+  }
+
+  const labelOption = {
+    color: '#FFFFFF',
+    font: '20px',
+  }
 })()
