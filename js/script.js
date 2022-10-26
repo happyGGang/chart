9c15afe75559fefd08cfe44ebe72d722ef4c0b11
happyGGang <joy.gang@dxworks.kr>
2022-10-26 22:55:30 +0900

    [update] getTotal 구현

--- js/script.js
+++ js/script.js
@@ -6,10 +6,31 @@
   }
 
   class Chart {
-    constructor() {
-
+    constructor(parent = 'body', data = {}, { width, hetght, radius, colors }) {
+      this.parent = get(parent)
+      this.canvas = document.createElement('canvas')
+      this.canvas.width = width
+      this.canvas.height = hetght
+      this.ctx = this.canvas.getContext('2d')
+      this.legends = document.createElement('div')
+      this.legends.classList.add('legends')
+      this.parent.appendChild(this.canvas)
+      this.parent.appendChild(this.legends)
+      this.label = ''
+      this.total = 0
+      this.datas = Object.entries(data)
+      this.radius = radius
+      this.colors = colors
     }
   }
+
+  const getTotal = () => {
+    for(const [data, value] of this.datas) {
+      this.total += value
+    }
+  }
+
+  
 
   const data = {
     guitar: 30,
@@ -21,7 +42,7 @@
   const option = {
     radius: 150,
     width: 700,
-    hetghit: 500,
+    hetght: 500,
     colors: ['#c15454', '#6fd971', '#687bd2', '#b971e0']
   }
 
@@ -31,5 +52,8 @@
   }
 
   const chart = new Chart('.canvas', data, option)
-  const { width, hetghit, radius, colors } = option
+  const { width, hetght, radius, colors } = option
+  chart.getTotal()
+  chart.drawlegends()
+  chart.drawChart(false, width / 2 - 10 -radius, hetght / 2, labelOption)
 })()
